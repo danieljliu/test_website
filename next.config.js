239958246a9a530a812271f7d901a49210774e40
danieljliu <danieljliu3@gmail.com>
2022-07-15 21:37:20 -0400

    changed basePath + assetPrefix to out

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages"
+  basePath: "/out/",
+  assetPrefix: "/out/"
 }
 
 module.exports = nextConfig
